e3a8cc04e2083614cd6c88dfc7cbcfb35d1038df
pratiksurya <67141608+pratiksurya@users.noreply.github.com>
2021-09-23 17:36:25 +0530

    Update Main.java

--- Question_4/src/question_4/Main.java
+++ Question_4/src/question_4/Main.java
@@ -1,3 +1,7 @@
+// Take integer inputs from user until he/she presses q ( Ask to press q to quit after every integer input ). 
+// Print average and product of all numbers.
+
+
 package question_4;
 
 import java.util.Scanner;
